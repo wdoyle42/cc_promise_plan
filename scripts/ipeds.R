155d779f61fdb1bb5b4c1bdae4f9045d089a3f78
btskinner <btskinner@coe.ufl.edu>
2021-09-13 13:42:45 -0400

    changes to use crosswalkr instead of noncensus, which is defunct

--- scripts/ipeds.R
+++ scripts/ipeds.R
@@ -24,14 +24,19 @@
 
 ##Libraries
 library(tidyverse)
-library(noncensus)
+library(crosswalkr)
 library(here)
 
 ## load functions
 source('functions.r')
 
+## directories
 rddir<-"../data/raw/"
 addir<-"../data/cleaned/"
+
+## create directories if they don't exist
+walk(c(rddir, addir), ~dir.create(.x, showWarnings = FALSE))
+
 ## =============================================================================
 ## BUILD DATASETS
 ## =============================================================================
@@ -114,12 +119,7 @@ inst<-
 ## Add full state names
 ## =============================================================================
 
-data(states)
-
-states<-states%>%
-  rename(stabbr=state)
-
-inst<-inst%>%left_join(states,by="stabbr")
+inst<-inst%>%left_join(stcrosswalk,by="stabbr") %>% rename(name = stname)
 
 ## =============================================================================
 ## Some misc cleanup
@@ -132,13 +132,10 @@ inst<-inst%>%
          unitid != 100636 ) ## drop cc of the airforce
 
 
-##Drop rownames
-rownames(inst) <- NULL
-
 ## Drop territories
 
 inst<-inst%>%
-  filter(!(is.na(region)))
+  filter(!(is.na(cenreg)))
 
 inst<-inst%>%mutate(tuition2=as.numeric(tuition2))
 
@@ -152,4 +149,4 @@ inst<-inst%>%filter(tribal==2)
 ## =============================================================================
 
 
-write_csv(inst, file = paste0(addir, 'institutions.csv'))
+write_csv(inst, file = file.path(addir, 'institutions.csv'))
